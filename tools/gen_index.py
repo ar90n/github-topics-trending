2e42aab9e0aa1e9587c78e9b2c41665de1ba8a6c
Masahiro Wada <argon.argon.argon@gmail.com>
2020-04-04 20:25:29 +0000

    Add tools

---
+++ tools/gen_index.py
@@ -0,0 +1,15 @@
+import sys
+from pathlib import Path
+import json
+
+if __name__ == "__main__":
+    try:
+        root_dir = sys.argv[1]
+    except:
+        sys.exit(-1)
+
+    result = {}
+    for p in Path(root_dir).glob('*/*/*.json'):
+        date, lang, f = str(p).split('/')[2:]
+        result.setdefault(date, {}).setdefault(lang, []).append(f)
+    print(json.dumps(result, indent=4))
